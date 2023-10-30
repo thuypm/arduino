b6d7c01811b58723400c76bf4a5ef95909774c13
Thuy Pham <pmthuymail@gmail.com>
2023-10-30 22:09:07 +0700

    create data

--- bin/socketServer.js
+++ bin/socketServer.js
@@ -1,11 +1,34 @@
 const io = require("socket.io");
+const { SerialPort } = require("serialport");
 
 const startSockerServer = (httpServer) => {
   const instance = io(httpServer);
-  instance.on("connection", (socket) => {
-    // socket.emit("send-data", "string");
-    socket.broadcast.emit("hello", "world");
-  });
+
+  // const arduinoSerialPort = new SerialPort({
+  //   path: process.env.COM_NAME || "COM6",
+  //   baudRate: Number(process.env.COM_BAURATE) || 9600,
+  // });
+  // arduinoSerialPort.on("open", function () {
+  //   console.log("Serial Port " + arduinoCOMPort + " is opened.");
+  //   arduinoSerialPort.on("data", (data) => {
+  //     console.log(data.toString());
+  //   });
+  // });
+
+  setInterval(() => {
+    const str = "fire:" + (Math.round(Math.random() * 1000) % 100);
+    const extractValue = str.split(":");
+    if (extractValue.length === 2) {
+      instance.emit("new-data", {
+        [extractValue[0]]: Number(extractValue[1]),
+      });
+    }
+  }, 1000);
+
+  //   instance.on("connection", (socket) => {
+  //     // socket.emit("send-data", "string");
+  //     socket.broadcast.emit("signal", "world");
+  //   });
 };
 module.exports = {
   startSockerServer,
