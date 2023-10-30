bc60769a6065c0fdbd16f132760df8e6ffd0081a
Thuy Pham <pmthuymail@gmail.com>
2023-10-30 22:11:13 +0700

    setup data to com port

--- bin/socketServer.js
+++ bin/socketServer.js
@@ -4,26 +4,32 @@ const { SerialPort } = require("serialport");
 const startSockerServer = (httpServer) => {
   const instance = io(httpServer);
 
-  // const arduinoSerialPort = new SerialPort({
-  //   path: process.env.COM_NAME || "COM6",
-  //   baudRate: Number(process.env.COM_BAURATE) || 9600,
-  // });
-  // arduinoSerialPort.on("open", function () {
-  //   console.log("Serial Port " + arduinoCOMPort + " is opened.");
-  //   arduinoSerialPort.on("data", (data) => {
-  //     console.log(data.toString());
-  //   });
-  // });
+  const arduinoSerialPort = new SerialPort({
+    path: process.env.COM_NAME || "COM6",
+    baudRate: Number(process.env.COM_BAURATE) || 9600,
+  });
+  arduinoSerialPort.on("open", function () {
+    console.log("Serial Port " + arduinoCOMPort + " is opened.");
+    arduinoSerialPort.on("data", (data) => {
+      const str = data.toString();
+      const extractValue = str.split(":");
+      if (extractValue.length === 2) {
+        instance.emit("new-data", {
+          [extractValue[0]]: Number(extractValue[1]),
+        });
+      }
+    });
+  });
 
-  setInterval(() => {
-    const str = "fire:" + (Math.round(Math.random() * 1000) % 100);
-    const extractValue = str.split(":");
-    if (extractValue.length === 2) {
-      instance.emit("new-data", {
-        [extractValue[0]]: Number(extractValue[1]),
-      });
-    }
-  }, 1000);
+  // setInterval(() => {
+  //   const str = "fire:" + (Math.round(Math.random() * 1000) % 100);
+  //   const extractValue = str.split(":");
+  //   if (extractValue.length === 2) {
+  //     instance.emit("new-data", {
+  //       [extractValue[0]]: Number(extractValue[1]),
+  //     });
+  //   }
+  // }, 1000);
 
   //   instance.on("connection", (socket) => {
   //     // socket.emit("send-data", "string");
